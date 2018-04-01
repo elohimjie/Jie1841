cae636faf6f22aab23cccdf791c82c4b3e5e9ac8
zhangjie <1310363529@qq.com>
2018-04-01 16:46:16 +0800

    jie1

---
+++ TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by zhangjie on 2018/4/1.
+//  Copyright © 2018年 zmt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by zhangjie on 2018/4/1.
+//  Copyright © 2018年 zmt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
